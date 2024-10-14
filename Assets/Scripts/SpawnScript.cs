823f262a6579cadffeb6d28e19d47119e2e23ba0
JaeYoun Kim <rlawodusx@naver.com>
2024-10-15 01:31:44 +0900

    Feat: AR 풍선 터뜨리기 Part - 02 진행중

---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+타겟을 생성하는 스크립트 
+*/
+public class SpawnScript : MonoBehaviour
+{
+    public Transform[] spawnPoints;     // 생성할 위치 
+    public GameObject[] balloons;       // 생성할 오브젝트 프리팹
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
