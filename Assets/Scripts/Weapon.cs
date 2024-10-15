61ea6385980988b7de728efb4113b4529c78603f
JaeYoun Kim <rlawodusx@naver.com>
2024-10-16 01:31:28 +0900

    Feat: 공격 이펙트 및 사운드 추가

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -10,8 +10,15 @@ using UnityEngine;
 */
 public class Weapon : MonoBehaviour
 {
+    [Header("Fire Effects")]
+    [SerializeField]
+    private GameObject muzzleFlashEffect;       // 총구 이펙트 (On / Off)
+
+
     [Header("Audio Clips")]
     [SerializeField] private AudioClip audioClipTakeOutWeapon;      // 무기 장착 사운드
+    [SerializeField]
+    private AudioClip audioClipFire;        // 공격 사운드 
 
     [Header("Weapon Setting")]
     [SerializeField]
@@ -33,6 +40,8 @@ public class Weapon : MonoBehaviour
     {
         // 무기 장착 사운드 재생
         PlaySound(audioClipTakeOutWeapon);
+        // 총구 이펙트 오브젝트 비활성화 
+        muzzleFlashEffect.SetActive(false);
     }
 
     public void StartWeaponAction()
@@ -53,6 +62,33 @@ public class Weapon : MonoBehaviour
         StopCoroutine("OnAttackLoop");
     }
 
+
+
+    private void OnAttack()
+    {
+        if (Time.time - lastAttackTime > weaponSetting.attackRate)
+        {
+            // 공격 주기가 되어야 공격할 수 있도록 하기 위해 현재 시간 저장
+            lastAttackTime = Time.time;
+
+            // 무기 애니메이션 재생(같은 애니메이션 반복 시 애니메이션을 끊고 처음부터 다시 재생)
+            animator.Play("Fire", -1, 0);
+
+            // 총구 이펙트 재생
+            StartCoroutine("OnMuzzleFlashEffect");
+
+            // 공격 사운드 재생 
+            PlaySound(audioClipFire);
+        }
+    }
+
+    private void PlaySound(AudioClip clip)
+    {
+        audioSource.Stop();     // 기존에 재생중인 사운드 정지 
+        audioSource.clip = clip;        // 새로운 사운드 clip으로 교체 
+        audioSource.Play();     // 사운드 재생 
+    }
+
     private IEnumerator OnAttackLoop()
     {
         // 실제 공격을 수행하는 OnAttack() 메소드를 매 프레임 실행 
@@ -64,22 +100,13 @@ public class Weapon : MonoBehaviour
         }
     }
 
-    private void OnAttack()
+    // 총구 이펙트 활성화 코루틴: 무기의 공격속도 보다 빠르게 muzzleFlashEffect를 잠깐 활성화한 후 비활성화 한다.
+    private IEnumerator OnMuzzleFlashEffect()
     {
-        if (Time.time - lastAttackTime > weaponSetting.attackRate)
-        {
-            // 공격 주기가 되어야 공격할 수 있도록 하기 위해 현재 시간 저장
-            lastAttackTime = Time.time;
+        muzzleFlashEffect.SetActive(true);
 
-            // 무기 애니메이션 재생(같은 애니메이션 반복 시 애니메이션을 끊고 처음부터 다시 재생)
-            animator.Play("Fire", -1, 0);
-        }
-    }
+        yield return new WaitForSeconds(weaponSetting.attackRate * 0.3f);
 
-    private void PlaySound(AudioClip clip)
-    {
-        audioSource.Stop();     // 기존에 재생중인 사운드 정지 
-        audioSource.clip = clip;        // 새로운 사운드 clip으로 교체 
-        audioSource.Play();     // 사운드 재생 
+        muzzleFlashEffect.SetActive(false);
     }
 }
